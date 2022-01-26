d68f983dd1f9ad1b20c6a9abf5399bc5e1758a3b
Bamee <thanida.jongarnon@exxonmobil.com>
2022-01-26 13:43:29 +0700

    Add logic for leap year that need to divide by 100 and 400

--- leapyear.py
+++ leapyear.py
@@ -5,6 +5,6 @@ class LeapYear:
         self.year = year
 
     def is_leap_year(self) -> bool:
-        if self.year % 4 == 0:
+        if self.year % 100 == 0 and self.year % 400 == 0:
             return True
         return False
