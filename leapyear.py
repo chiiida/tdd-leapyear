a6ec147207e66e2286e345adb8346d826a019387
Sirikonss <macintoshhd@Macintoshs-MacBook-Pro.local>
2022-01-26 13:24:30 +0700

    Add logic for 2004

--- leapyear.py
+++ leapyear.py
@@ -7,4 +7,6 @@ class LeapYear:
     def is_leap_year(self) -> bool:
         if self.year == 2000:
             return True
+        if self.year == 2004:
+            return True
         return False
